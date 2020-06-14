fac59a3651d419f8897be44784676545602637bf
gustiandra <gustiandral21@gmail.com>
2020-06-14 09:57:17 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Dari local ke github</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis maiores ipsum, assumenda magnam ipsam hic esse, deserunt ab ad, aspernatur nobis quasi enim vitae optio vel quos illo alias perferendis.</p>
 </body>
 </html>
